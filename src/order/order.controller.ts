9fdfc43f76d0c6d1a952afa9377f0aa0ee8b4b39
Andrey Dremin <sankoa230@gmail.com>
2024-05-21 08:59:13 +0300

    feat: added findMany orders endpoint

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { CurrentUserId } from 'src/auth/decorators/userId.decorator';
 import { CreateOrderDto, UpdateOrderDto } from './dto';
 import { OrderService } from './order.service';
+import { Order } from '@prisma/client';
 
 @Controller('order')
 export class OrderController {
@@ -13,9 +14,14 @@ export class OrderController {
         return await this.orderService.create(id,body.text)
     }
 
-    @Put("orderId")
-    async updateOne(@Param("orderId") orderId : string,@Body() {status,message}: UpdateOrderDto){
+    @Put(":orderId")
+    async updateOne(@Param("orderId") orderId : string,@Body() {status,message}: UpdateOrderDto):Promise<Order>{
         return await this.orderService.updateOne(orderId,status,message)
+    }
+
+    @Get("")
+    async findMany(@CurrentUserId() id : string): Promise<Order[]> {
+      return await this.orderService.findManyByUserId(id)
     }
 
 }
